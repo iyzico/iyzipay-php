00108ab9e68ae131cd9982ec63c308555ba1a298
Sabri Onur Tüzün <onur.tuzun@iyzico.com>
2016-02-22 01:01:09 +0200

    composer updates

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,19 +1,4 @@
 <?php
 
-function includeIfExists($file)
-{
-    if (file_exists($file)) {
-        return include $file;
-    }
-    return '';
-}
-
-if ((!$loader = includeIfExists(__DIR__ . '/../vendor/autoload.php')) && (!$loader = includeIfExists(__DIR__ . '/../../../.composer/autoload.php'))) {
-    die ('You must set up the project dependencies, run the following commands:' .
-        PHP_EOL . 'curl -s http://getcomposer.org/installer | php' .
-        PHP_EOL . 'php composer.phar install' . PHP_EOL);
-}
-
-$loader->add('Iyzipay\Tests', __DIR__);
-
-return $loader;
+$loader = require dirname(__DIR__) . '/vendor/autoload.php';
+$loader->addPsr4('Iyzipay\\Tests\\', __DIR__ . '/');
