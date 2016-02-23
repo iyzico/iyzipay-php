fdca6d4f311587746ec9b225c37a8344466ae4fa
Sabri Onur Tüzün <onur.tuzun@iyzico.com>
2016-02-23 14:21:29 +0200

    payment retrieve resource methods added

--- src/Iyzipay/Model/PaymentAuth.php
+++ src/Iyzipay/Model/PaymentAuth.php
@@ -7,12 +7,19 @@ use Iyzipay\JsonBuilder;
 use Iyzipay\Model\Mapper\PaymentAuthMapper;
 use Iyzipay\Options;
 use Iyzipay\Request\CreatePaymentRequest;
+use Iyzipay\Request\RetrievePaymentRequest;
 
 class PaymentAuth extends Payment
 {
     public static function create(CreatePaymentRequest $request, Options $options)
     {
         $rawResult = HttpClient::create()->post($options->getBaseUrl() . "/payment/iyzipos/auth/ecom", parent::getHttpHeaders($request, $options), $request->toJsonString());
+        return PaymentAuthMapper::create()->mapPaymentAuth(new PaymentAuth(), JsonBuilder::jsonDecode($rawResult));
+    }
+
+    public static function retrieve(RetrievePaymentRequest $request, Options $options)
+    {
+        $rawResult = HttpClient::create()->post($options->getBaseUrl() . "/payment/detail", parent::getHttpHeaders($request, $options), $request->toJsonString());
         return PaymentAuthMapper::create()->mapPaymentAuth(new PaymentAuth(), JsonBuilder::jsonDecode($rawResult));
     }
 }
--- src/Iyzipay/Model/ThreeDSAuth.php
+++ src/Iyzipay/Model/ThreeDSAuth.php
@@ -7,12 +7,19 @@ use Iyzipay\JsonBuilder;
 use Iyzipay\Model\Mapper\ThreeDSAuthMapper;
 use Iyzipay\Options;
 use Iyzipay\Request\CreateThreeDSAuthRequest;
+use Iyzipay\Request\RetrievePaymentRequest;
 
 class ThreeDSAuth extends Payment
 {
     public static function create(CreateThreeDSAuthRequest $request, Options $options)
     {
         $rawResult = HttpClient::create()->post($options->getBaseUrl() . "/payment/iyzipos/auth3ds/ecom", parent::getHttpHeaders($request, $options), $request->toJsonString());
+        return ThreeDSAuthMapper::create()->mapThreeDSAuth(new ThreeDSAuth(), JsonBuilder::jsonDecode($rawResult));
+    }
+
+    public static function retrieve(RetrievePaymentRequest $request, Options $options)
+    {
+        $rawResult = HttpClient::create()->post($options->getBaseUrl() . "/payment/detail", parent::getHttpHeaders($request, $options), $request->toJsonString());
         return ThreeDSAuthMapper::create()->mapThreeDSAuth(new ThreeDSAuth(), JsonBuilder::jsonDecode($rawResult));
     }
 }
