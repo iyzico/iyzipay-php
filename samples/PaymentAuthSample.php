805f2f6a17ca185b2d4b49729a5dcf6ad12204de
anil.eray <anil.eray@iyzico.com>
2016-02-26 10:40:56 +0200

    #1 should_create_payment_with_physical_and_virtual_item_for_standard_merchan sample added

--- samples/PaymentAuthSample.php
+++ samples/PaymentAuthSample.php
@@ -6,12 +6,103 @@ require_once('Sample.php');
 IyzipayBootstrap::init();
 
 $sample = new PaymentAuthSample();
+$sample->should_create_payment_with_physical_and_virtual_item_for_standard_merchant();
 $sample->should_create_payment_with_physical_and_virtual_item_for_market_place();
 $sample->should_create_payment_with_physical_and_virtual_item_for_listing_or_subscription();
 $sample->should_retrieve_payment();
 
 class PaymentAuthSample
 {
+    public function should_create_payment_with_physical_and_virtual_item_for_standard_merchant()
+    {
+        # create request class
+        $request = new \Iyzipay\Request\CreatePaymentRequest();
+        $request->setLocale(\Iyzipay\Model\Locale::TR);
+        $request->setConversationId("123456789");
+        $request->setPrice("1");
+        $request->setPaidPrice("1.1");
+        $request->setInstallment(1);
+        $request->setBasketId("B67832");
+        $request->setPaymentChannel(\Iyzipay\Model\PaymentChannel::WEB);
+        $request->setPaymentGroup(\Iyzipay\Model\PaymentGroup::PRODUCT);
+
+        $paymentCard = new \Iyzipay\Model\PaymentCard();
+        $paymentCard->setCardHolderName("John Doe");
+        $paymentCard->setCardNumber("5528790000000008");
+        $paymentCard->setExpireMonth("12");
+        $paymentCard->setExpireYear("2030");
+        $paymentCard->setCvc("123");
+        $paymentCard->setRegisterCard(0);
+        $request->setPaymentCard($paymentCard);
+
+        $buyer = new \Iyzipay\Model\Buyer();
+        $buyer->setId("BY789");
+        $buyer->setName("John");
+        $buyer->setSurname("Doe");
+        $buyer->setGsmNumber("+905350000000");
+        $buyer->setEmail("email@email.com");
+        $buyer->setIdentityNumber("74300864791");
+        $buyer->setLastLoginDate("2015-10-05 12:43:35");
+        $buyer->setRegistrationDate("2013-04-21 15:12:09");
+        $buyer->setRegistrationAddress("Address");
+        $buyer->setIp("85.34.78.112");
+        $buyer->setCity("Istanbul");
+        $buyer->setCountry("Turkey");
+        $buyer->setZipCode("34732");
+        $request->setBuyer($buyer);
+
+        $shippingAddress = new \Iyzipay\Model\Address();
+        $shippingAddress->setContactName("Jane Doe");
+        $shippingAddress->setCity("Istanbul");
+        $shippingAddress->setCountry("Turkey");
+        $shippingAddress->setAddress("Address");
+        $shippingAddress->setZipCode("34742");
+        $request->setShippingAddress($shippingAddress);
+
+        $billingAddress = new \Iyzipay\Model\Address();
+        $billingAddress->setContactName("Jane Doe");
+        $billingAddress->setCity("Istanbul");
+        $billingAddress->setCountry("Turkey");
+        $billingAddress->setAddress("Address");
+        $billingAddress->setZipCode("34742");
+        $request->setbillingAddress($billingAddress);
+
+        $basketItems = array();
+        $firstBasketItem = new \Iyzipay\Model\BasketItem();
+        $firstBasketItem->setId("BI101");
+        $firstBasketItem->setName("Binocular");
+        $firstBasketItem->setCategory1("Collectibles");
+        $firstBasketItem->setCategory2("Accessories");
+        $firstBasketItem->setItemType(\Iyzipay\Model\BasketItemType::PHYSICAL);
+        $firstBasketItem->setPrice("0.3");
+        $basketItems[0] = $firstBasketItem;
+
+        $secondBasketItem = new \Iyzipay\Model\BasketItem();
+        $secondBasketItem->setId("BI102");
+        $secondBasketItem->setName("Game code");
+        $secondBasketItem->setCategory1("Game");
+        $secondBasketItem->setCategory2("Online Game Items");
+        $secondBasketItem->setItemType(\Iyzipay\Model\BasketItemType::VIRTUAL);
+        $secondBasketItem->setPrice("0.5");
+        $basketItems[1] = $secondBasketItem;
+
+        $thirdBasketItem = new \Iyzipay\Model\BasketItem();
+        $thirdBasketItem->setId("BI103");
+        $thirdBasketItem->setName("Usb");
+        $thirdBasketItem->setCategory1("Electronics");
+        $thirdBasketItem->setCategory2("Usb / Cable");
+        $thirdBasketItem->setItemType(\Iyzipay\Model\BasketItemType::PHYSICAL);
+        $thirdBasketItem->setPrice("0.2");
+        $basketItems[2] = $thirdBasketItem;
+        $request->setBasketItems($basketItems);
+
+        # make request
+        $paymentAuth = \Iyzipay\Model\PaymentAuth::create($request, Sample::options());
+
+        # print response
+        print_r($paymentAuth);
+    }
+
     public function should_create_payment_with_physical_and_virtual_item_for_market_place()
     {
         # create request class
