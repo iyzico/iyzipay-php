1aa5d4dcf2a1e6eb1c9476116992273a82c64207
Sabri Onur Tüzün <onur.tuzun@iyzico.com>
2016-03-19 12:02:12 +0200

    approval request test written

--- tests/Iyzipay/Tests/Request/CreateApprovalRequestTest.php
+++ tests/Iyzipay/Tests/Request/CreateApprovalRequestTest.php
@@ -3,44 +3,50 @@
 namespace Iyzipay\Tests;
 
 use Iyzipay\Model\Locale;
-use Iyzipay\Request;
+use Iyzipay\Request\CreateApprovalRequest;
 
-class RequestTest extends BaseTest
+class CreateApprovalRequestTest extends BaseTest
 {
     public function test_should_get_json_object()
     {
-        $request = new Request();
+        $request = new CreateApprovalRequest();
         $request->setLocale(Locale::TR);
         $request->setConversationId("123456");
+        $request->setPaymentTransactionId("1");
 
         $jsonObject = $request->getJsonObject();
 
         $this->assertEquals(Locale::TR, $jsonObject["locale"]);
         $this->assertEquals("123456", $jsonObject["conversationId"]);
+        $this->assertEquals("1", $jsonObject["paymentTransactionId"]);
     }
 
     public function test_should_convert_to_pki_request_string()
     {
-        $request = new Request();
+        $request = new CreateApprovalRequest();
         $request->setLocale(Locale::TR);
         $request->setConversationId("123456");
+        $request->setPaymentTransactionId("1");
 
         $str = "[locale=tr," .
-            "conversationId=123456]";
+            "conversationId=123456," .
+            "paymentTransactionId=1]";
 
         $this->assertEquals($str, $request->toPKIRequestString());
     }
 
     public function test_should_get_json_string()
     {
-        $request = new Request();
+        $request = new CreateApprovalRequest();
         $request->setLocale(Locale::TR);
         $request->setConversationId("123456");
+        $request->setPaymentTransactionId("1");
 
         $json = '
             {
                 "locale":"tr",
-                "conversationId":"123456"
+                "conversationId":"123456",
+                "paymentTransactionId":"1"
             }';
 
         $this->assertJson($request->toJsonString());
