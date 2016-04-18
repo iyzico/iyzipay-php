e4e182fc072806793c042d9fc9324731ae11de4a
kurtulus <kurtulussahin@gmail.com>
2016-04-18 03:04:44 +0300

    Create payment post auth request test added

---
+++ tests/Iyzipay/Tests/Request/CreatePaymentPostAuthRequestTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Iyzipay\Tests\Request;
+
+use Iyzipay\Model\Locale;
+use Iyzipay\Request\CreatePaymentPostAuthRequest;
+use Iyzipay\Tests\TestCase;
+
+class CreatePaymentPostAuthRequestTest extends TestCase
+{
+    public function test_should_get_json_object()
+    {
+        $request = new CreatePaymentPostAuthRequest();
+        $request->setLocale(Locale::TR);
+        $request->setConversationId("123456");
+        $request->setPaymentId("1");
+        $request->setPaidPrice("0.6");
+        $request->setIp("85.34.78.112");
+
+        $jsonObject = $request->getJsonObject();
+
+        $this->assertEquals(Locale::TR, $jsonObject["locale"]);
+        $this->assertEquals("123456", $jsonObject["conversationId"]);
+        $this->assertEquals("1", $jsonObject["paymentId"]);
+        $this->assertEquals("0.6", $jsonObject["paidPrice"]);
+        $this->assertEquals("85.34.78.112", $jsonObject["ip"]);
+    }
+
+    public function test_should_convert_to_pki_request_string()
+    {
+        $request = new CreatePaymentPostAuthRequest();
+        $request->setLocale(Locale::TR);
+        $request->setConversationId("123456");
+        $request->setPaymentId("1");
+        $request->setPaidPrice("0.6");
+        $request->setIp("85.34.78.112");
+
+        $str = "[locale=tr," .
+            "conversationId=123456," .
+            "paymentId=1," .
+            "paidPrice=0.6," .
+            "ip=85.34.78.112]";
+
+        $this->assertEquals($str, $request->toPKIRequestString());
+    }
+
+    public function test_should_get_json_string()
+    {
+        $request = new CreatePaymentPostAuthRequest();
+        $request->setLocale(Locale::TR);
+        $request->setConversationId("123456");
+        $request->setPaymentId("1");
+        $request->setPaidPrice("0.6");
+        $request->setIp("85.34.78.112");
+
+        $json = '
+            {
+                "locale":"tr",
+                "conversationId":"123456",
+                "paymentId":"1",
+                "paidPrice":"0.6",
+                "ip":"85.34.78.112"
+            }';
+
+        $this->assertJson($request->toJsonString());
+        $this->assertJsonStringEqualsJsonString($json, $request->toJsonString());
+    }
+}
