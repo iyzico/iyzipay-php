735ea6af664b236c18ace777d5a47fd6a28b81ea
kurtulussahin <kurtulussahin@gmail.com>
2016-04-19 23:14:46 +0000

    Retrieve Payment Request Test added

---
+++ tests/Iyzipay/Tests/Request/RetrievePaymentRequestTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Iyzipay\Tests\Request;
+
+use Iyzipay\Model\Locale;
+use Iyzipay\Request\RetrievePaymentRequest;
+use Iyzipay\Tests\TestCase;
+
+class RetrievePaymentRequestTest extends TestCase
+{
+    public function test_should_get_json_object()
+    {
+        $request = new RetrievePaymentRequest();
+        $request->setLocale(Locale::TR);
+        $request->setConversationId("123456789");
+        $request->setPaymentId("1");
+        $request->setPaymentConversationId("123456789");
+
+        $jsonObject = $request->getJsonObject();
+
+        $this->assertEquals(Locale::TR, $jsonObject["locale"]);
+        $this->assertEquals("123456789", $jsonObject["conversationId"]);
+        $this->assertEquals("1", $jsonObject["paymentId"]);
+        $this->assertEquals("123456789", $jsonObject["paymentConversationId"]);
+    }
+
+    public function test_should_convert_to_pki_request_string()
+    {
+        $request = new RetrievePaymentRequest();
+        $request->setLocale(Locale::TR);
+        $request->setConversationId("123456789");
+        $request->setPaymentId("1");
+        $request->setPaymentConversationId("123456789");
+
+        $str = "[locale=tr," .
+            "conversationId=123456789," .
+            "paymentId=1," .
+            "paymentConversationId=123456789]";
+
+        $this->assertEquals($str, $request->toPKIRequestString());
+    }
+
+    public function test_should_get_json_string()
+    {
+        $request = new RetrievePaymentRequest();
+        $request->setLocale(Locale::TR);
+        $request->setConversationId("123456789");
+        $request->setPaymentId("1");
+        $request->setPaymentConversationId("123456789");
+
+        $json = '
+            {
+                "locale":"tr",
+                "conversationId":"123456789",
+                "paymentId":"1",
+                "paymentConversationId":"123456789"
+            }';
+
+        $this->assertJson($request->toJsonString());
+        $this->assertJsonStringEqualsJsonString($json, $request->toJsonString());
+    }
+}
