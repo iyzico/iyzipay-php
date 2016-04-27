ceb6939e27e25c7153186c3d3407b04a1fd95084
kurtulussahin <kurtulussahin@gmail.com>
2016-04-27 16:19:01 +0000

    BKM Initialize Mapper Test added

---
+++ tests/Iyzipay/Tests/Model/Mapper/BKMInitializeMapperTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Iyzipay\Tests\Model\Mapper;
+
+use Iyzipay\Model\BKMInitialize;
+use Iyzipay\Model\Locale;
+use Iyzipay\Model\Mapper\BKMInitializeMapper;
+use Iyzipay\Model\Status;
+use Iyzipay\Tests\TestCase;
+
+class BKMInitializeMapperTest extends TestCase
+{
+    public function test_should_map_BKM_Initialize_number_success_raw_result()
+    {
+        $json = '
+            {   
+                "status":"success",
+                "errorCode":null,
+                "errorMessage":null,
+                "errorGroup":null,
+                "locale":"tr",
+                "systemTime":"1458545234852",
+                "conversationId":"123456",
+                "htmlContent":"html content",
+                "token":"token"
+            }';
+
+        $BKMInitialize = BKMInitializeMapper::create($json)->jsonDecode()->mapBKMInitialize(new BKMInitialize());
+
+        $this->assertNotEmpty($BKMInitialize);
+        $this->assertEquals(Status::SUCCESS, $BKMInitialize->getStatus());
+        $this->assertEmpty($BKMInitialize->getErrorCode());
+        $this->assertEmpty($BKMInitialize->getErrorMessage());
+        $this->assertEmpty($BKMInitialize->getErrorGroup());
+        $this->assertEquals(Locale::TR, $BKMInitialize->getLocale());
+        $this->assertEquals("1458545234852", $BKMInitialize->getSystemTime());
+        $this->assertEquals("123456", $BKMInitialize->getConversationId());
+        $this->assertJson($BKMInitialize->getRawResult());
+        $this->assertJsonStringEqualsJsonString($json, $BKMInitialize->getRawResult());
+        $this->assertEquals(base64_decode("html content"), $BKMInitialize->getHtmlContent());
+        $this->assertEquals("token", $BKMInitialize->getToken());
+    }
+}
