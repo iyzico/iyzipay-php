28391c6e8d400d0fd30513f18707e73e85c624f1
kurtulussahin <kurtulussahin@gmail.com>
2016-06-16 09:29:26 +0000

    pecco implemented

---
+++ tests/Iyzipay/Tests/Request/CreatePeccoPaymentRequestTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Iyzipay\Tests\Request;
+
+use Iyzipay\Model\Locale;
+use Iyzipay\Request\CreatePeccoPaymentRequest;
+use Iyzipay\Tests\TestCase;
+
+class CreatePeccoPaymentRequestTest extends TestCase
+{
+    public function test_should_get_json_object()
+    {
+        $request = new CreatePeccoPaymentRequest();
+        $request->setLocale(Locale::TR);
+        $request->setConversationId("123456789");
+        $request->setToken("token");
+
+        $jsonObject = $request->getJsonObject();
+
+        $this->assertEquals(Locale::TR, $jsonObject["locale"]);
+        $this->assertEquals("123456789", $jsonObject["conversationId"]);
+        $this->assertEquals("token", $jsonObject["token"]);
+    }
+
+    public function test_should_convert_to_pki_request_string()
+    {
+        $request = new CreatePeccoPaymentRequest();
+        $request->setLocale(Locale::TR);
+        $request->setConversationId("123456789");
+        $request->setToken("token");
+
+        $str = "[locale=tr," .
+            "conversationId=123456789," .
+            "token=token]";
+
+        $this->assertEquals($str, $request->toPKIRequestString());
+    }
+
+    public function test_should_get_json_string()
+    {
+        $request = new CreatePeccoPaymentRequest();
+        $request->setLocale(Locale::TR);
+        $request->setConversationId("123456789");
+        $request->setToken("token");
+
+        $json = '
+            {
+                "locale":"tr",
+                "conversationId":"123456789",
+                "token":"token"
+            }';
+
+        $this->assertJson($request->toJsonString());
+        $this->assertJsonStringEqualsJsonString($json, $request->toJsonString());
+    }
+}
