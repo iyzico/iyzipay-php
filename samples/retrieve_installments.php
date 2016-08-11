86bc59189247e0308c00722d2c6709808ef48818
Sabri Onur Tüzün <onur.tuzun@iyzico.com>
2016-08-11 03:41:39 +0300

    bin changed in samples

--- samples/retrieve_installments.php
+++ samples/retrieve_installments.php
@@ -7,7 +7,7 @@ $request = new \Iyzipay\Request\RetrieveInstallmentInfoRequest();
 $request->setLocale(\Iyzipay\Model\Locale::TR);
 $request->setConversationId("123456789");
 $request->setBinNumber("554960");
-$request->setPrice("1");
+$request->setPrice("100");
 
 # make request
 $installmentInfo = \Iyzipay\Model\InstallmentInfo::retrieve($request, Config::options());
