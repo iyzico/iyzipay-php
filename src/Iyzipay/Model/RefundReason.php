2b31ee3184aa6b5414c7ddaf22a5b74f65c0e6d1
Kemal Toprak Ucar <toprak.ucar@iyzico.com>
2017-12-18 17:52:24 +0300

    enum values are changed as their names

--- src/Iyzipay/Model/RefundReason.php
+++ src/Iyzipay/Model/RefundReason.php
@@ -4,8 +4,8 @@ namespace Iyzipay\Model;
 
 class RefundReason
 {
-    const DOUBLE_PAYMENT = "double payment";
-    const BUYER_REQUEST = "buyer request";
+    const DOUBLE_PAYMENT = "double_payment";
+    const BUYER_REQUEST = "buyer_request";
     const FRAUD = "fraud";
     const OTHER = "other";
 }
