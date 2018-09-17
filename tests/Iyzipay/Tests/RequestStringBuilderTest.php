a864ee87402aa8c7d8bf752032e59f9c55c35e57
Mehmet Selim Dil <mehmetselimdil@gmail.com>
2018-09-17 15:55:14 +0300

    RequestStringQuery pages testcase added.

--- tests/Iyzipay/Tests/RequestStringBuilderTest.php
+++ tests/Iyzipay/Tests/RequestStringBuilderTest.php
@@ -13,8 +13,20 @@ class RequestStringBuilderTest extends TestCase
         $request = new Request();
         $request->setLocale(Locale::TR);
         $request->setConversationId("123456789");
-        $result = RequestStringBuilder::requestToStringQuery($request, $type = null);
+        $result = RequestStringBuilder::requestToStringQuery($request, null);
 
         $this->assertEquals("?conversationId=123456789&locale=tr", $result);
+    }
+
+    public function test_should_request_string_query_pages()
+    {
+        $request = new \Iyzipay\Request\Iyzilink\IyziLinkRetriveAllPagininRequest();
+        $request->setLocale(Locale::TR);
+        $request->setConversationId("123456789");
+        $request->setPage(1);
+        $request->setCount(2);
+        $result = RequestStringBuilder::requestToStringQuery($request, 'pages');
+
+        $this->assertEquals("?conversationId=123456789&locale=tr&page=1&count=2", $result);
     }
 }
