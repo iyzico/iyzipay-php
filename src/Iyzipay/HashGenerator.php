0fe17f8187bf17d40c98b0faf5f2881eeca697dd
Mehmet Selim Dil <mehmetselimdil@gmail.com>
2018-09-17 15:30:43 +0300

    Hashgenerator remove func.

--- src/Iyzipay/HashGenerator.php
+++ src/Iyzipay/HashGenerator.php
@@ -9,39 +9,4 @@ class HashGenerator
         $hashStr = $apiKey . $randomString . $secretKey . $request->toPKIRequestString();
         return base64_encode(sha1($hashStr, true));
     }
-
-    public static function generateHashV2($uri, $apiKey, $secretKey, $randomString, Request $request = null)
-    {
-        $hashStr = "apiKey:" . $apiKey . "&randomKey:" . $randomString ."&signature:" . self::getHmacSHA256Signature($uri, $secretKey, $randomString, $request);
-
-        $hashStr = base64_encode($hashStr);
-
-        return $hashStr;
-    }
-
-
-    public static function getHmacSHA256Signature($uri, $secretKey, $randomString, Request $request = null)
-    {
-        $dataToEncrypt = $randomString . self::getPayload($uri, $request);
-
-        $hash = hash_hmac('sha256', $dataToEncrypt, $secretKey, true);
-        $token = bin2hex($hash);
-
-        return $token;
-    }
-
-    public static function getPayload($uri, Request $request = null)
-    {
-
-        $startNumber  = strpos($uri, '/v2');
-        $endNumber    = strpos($uri, '?');
-        $endNumber-=  $startNumber;
-
-        $uriPath      =  substr($uri, $startNumber, $endNumber);
-
-        if (!empty($request) && $request->toJsonString() != '[]')
-            $uriPath = $uriPath.$request->toJsonString();
-
-        return $uriPath;
-    }
 }
