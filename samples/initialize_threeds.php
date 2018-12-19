858d20cb4fdcefd48f07227a0f429354e5b163ff
emre faik <emrefaik@gmail.com>
2018-12-19 15:21:43 +0300

    address update
    sample address updated

--- samples/initialize_threeds.php
+++ samples/initialize_threeds.php
@@ -33,7 +33,7 @@ $buyer->setEmail("email@email.com");
 $buyer->setIdentityNumber("74300864791");
 $buyer->setLastLoginDate("2015-10-05 12:43:35");
 $buyer->setRegistrationDate("2013-04-21 15:12:09");
-$buyer->setRegistrationAddress("Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1");
+$buyer->setRegistrationAddress("Burhaniye Mahallesi Atilla Sokak No:7 Üsküdar");
 $buyer->setIp("85.34.78.112");
 $buyer->setCity("Istanbul");
 $buyer->setCountry("Turkey");
@@ -44,7 +44,7 @@ $shippingAddress = new \Iyzipay\Model\Address();
 $shippingAddress->setContactName("Jane Doe");
 $shippingAddress->setCity("Istanbul");
 $shippingAddress->setCountry("Turkey");
-$shippingAddress->setAddress("Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1");
+$shippingAddress->setAddress("Burhaniye Mahallesi Atilla Sokak No:7 Üsküdar");
 $shippingAddress->setZipCode("34742");
 $request->setShippingAddress($shippingAddress);
 
@@ -52,7 +52,7 @@ $billingAddress = new \Iyzipay\Model\Address();
 $billingAddress->setContactName("Jane Doe");
 $billingAddress->setCity("Istanbul");
 $billingAddress->setCountry("Turkey");
-$billingAddress->setAddress("Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1");
+$billingAddress->setAddress("Burhaniye Mahallesi Atilla Sokak No:7 Üsküdar");
 $billingAddress->setZipCode("34742");
 $request->setBillingAddress($billingAddress);
 
