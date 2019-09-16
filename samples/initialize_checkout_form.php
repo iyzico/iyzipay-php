1e31a4949a47deb33fcf3f98267221189eadc02c
Mehmet Selim Dil <mehmetselimdil@gmail.com>
2019-09-16 16:10:44 +0300

    update

--- samples/initialize_checkout_form.php
+++ samples/initialize_checkout_form.php
@@ -11,7 +11,7 @@ $request->setPaidPrice("1.2");
 $request->setCurrency(\Iyzipay\Model\Currency::TL);
 $request->setBasketId("B67832");
 $request->setPaymentGroup(\Iyzipay\Model\PaymentGroup::PRODUCT);
-$request->setCallbackUrl("http://localhost:8888/iyzipay/iyzipay-php/samples/retrieve_checkout_form_result.php");
+$request->setCallbackUrl("https://www.merchant.com/callback");
 $request->setEnabledInstallments(array(2, 3, 6, 9));
 
 $buyer = new \Iyzipay\Model\Buyer();
