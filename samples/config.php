f6de9fd6a88e335436119febadc3390680088713
Mehmet Selim Dil <mehmetselimdil@gmail.com>
2019-10-10 16:22:43 +0300

    add refund to balance test

--- samples/config.php
+++ samples/config.php
@@ -9,9 +9,9 @@ class Config
     public static function options()
     {
         $options = new \Iyzipay\Options();
-        $options->setApiKey("IJTgyxLBidM8Hj2G8c4MVUdrIbgh6ZVz");
-        $options->setSecretKey("IdSUuKN4lumCl3QU17NAiJ8386Zms4hb");
-        $options->setBaseUrl("https://api.iyzipay.com");
+        $options->setApiKey("api-key");
+        $options->setSecretKey("secret-key");
+        $options->setBaseUrl("https://sandbox-api.iyzipay.com");
         return $options;
     }
 }
