d9249bf57d4e13d4fac1ece1aa7a2cf8e3b00cbd
eroluysal <eutkuuysal@gmail.com>
2021-03-23 23:29:34 +0300

    Update Currency.php

--- src/Iyzipay/Model/Currency.php
+++ src/Iyzipay/Model/Currency.php
@@ -4,12 +4,15 @@ namespace Iyzipay\Model;
 
 class Currency
 {
-    const TL = "TRY";
-    const EUR = "EUR";
-    const USD = "USD";
-    const GBP = "GBP";
-    const IRR = "IRR";
-    const NOK = "NOK";
-    const RUB = "RUB";
-    const CHF = "CHF";
+    const TRY = 'TRY';
+    const EUR = 'EUR';
+    const USD = 'USD';
+    const GBP = 'GBP';
+    const IRR = 'IRR';
+    const NOK = 'NOK';
+    const RUB = 'RUB';
+    const CHF = 'CHF';
+
+    // Alias of TRY
+    const TL = self::TRY;
 }
