f1b12750bb88c34505cbbdde9c71bf172c182de6
Osman Keser <osman@basefy.com>
2024-07-04 16:24:28 +0300

    Function updated to reflect params order variations

--- samples/signature_verification.php
+++ samples/signature_verification.php
@@ -1,14 +1,9 @@
 <?php
 
-function calculateHmacSHA256Signature($paymentId, $redirectUrl, $currency, $basketId, $conversationId, $paidPrice, $price, $token, $secretKey) {
-    $params = [$paymentId, $redirectUrl, $currency, $basketId, $conversationId, $paidPrice, $price, $token];
-    $dataToSignArray = array();
-
-    foreach ($params as $param) {
-        $param && $dataToSignArray[] = $param;
-    }
-
-    $dataToSign = implode(':', $dataToSignArray);
+function calculateHmacSHA256Signature($params)
+{
+    $secretKey = Config::options()->getSecretKey();
+    $dataToSign = implode(':', $params);
     $mac = hash_hmac('sha256', $dataToSign, $secretKey, true);
     return bin2hex($mac);
 }
