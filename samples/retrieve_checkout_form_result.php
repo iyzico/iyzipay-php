d5957a3b87466414526f31568180b9076dabda5e
Osman Keser <osman@basefy.com>
2024-07-04 17:33:38 +0300

    CheckoutForm Retrieve service updated to v2 Headers

--- samples/retrieve_checkout_form_result.php
+++ samples/retrieve_checkout_form_result.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once('config.php');
+require_once('signature_verification.php');
 
 # create request class
 $request = new \Iyzipay\Request\RetrieveCheckoutFormRequest();
@@ -13,3 +14,18 @@ $checkoutForm = \Iyzipay\Model\CheckoutForm::retrieve($request, Config::options(
 
 # print result
 print_r($checkoutForm);
+
+#verify signature
+$paymentStatus = $checkoutForm->getPaymentStatus();
+$paymentId = $checkoutForm->getPaymentId();
+$currency = $checkoutForm->getCurrency();
+$basketId = $checkoutForm->getBasketId();
+$conversationId = $checkoutForm->getConversationId();
+$paidPrice = $checkoutForm->getPaidPrice();
+$price = $checkoutForm->getPrice();
+$token = $checkoutForm->getToken();
+$signature = $checkoutForm->getSignature();
+
+$calculatedSignature = calculateHmacSHA256Signature(array($paymentStatus, $paymentId, $currency, $basketId, $conversationId, $paidPrice, $price, $token));
+$verified = $signature == $calculatedSignature;
+echo "Signature verified: $verified";
