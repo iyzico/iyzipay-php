d2339046bb2d9ecddc30b85ca7711ec9a93662a6
Osman Keser <osman@basefy.com>
2024-07-04 20:15:42 +0300

    Provision Service Initialize CheckoutForm Preauth updated to v2 headers along with a sample file

--- src/Iyzipay/Model/CheckoutFormInitializePreAuth.php
+++ src/Iyzipay/Model/CheckoutFormInitializePreAuth.php
@@ -10,7 +10,8 @@ class CheckoutFormInitializePreAuth extends CheckoutFormInitializeResource
 {
     public static function create(CreateCheckoutFormInitializeRequest $request, Options $options)
     {
-        $rawResult = parent::httpClient()->post($options->getBaseUrl() . "/payment/iyzipos/checkoutform/initialize/preauth/ecom", parent::getHttpHeaders($request, $options), $request->toJsonString());
+        $uri = "/payment/iyzipos/checkoutform/initialize/preauth/ecom";
+        $rawResult = parent::httpClient()->post($options->getBaseUrl() . $uri, parent::getHttpHeadersV2($uri, $request, $options), $request->toJsonString());
         return CheckoutFormInitializePreAuthMapper::create($rawResult)->jsonDecode()->mapCheckoutFormInitializePreAuth(new CheckoutFormInitializePreAuth());
     }
 }
