026154d8151be625f7a1e8de09f900bc1ba3e265
Osman Keser <osman@basefy.com>
2025-12-15 12:33:21 +0300

    Refactor `IyzipayResource` to remove v1 auth methods and for improved authorization handling and test coverage adjustments. Removed unnecessary XML configurations.

--- tests/Iyzipay/Tests/IyzipayResourceTest.php
+++ tests/Iyzipay/Tests/IyzipayResourceTest.php
@@ -5,34 +5,37 @@ namespace Iyzipay\Tests;
 use Iyzipay\IyzipayResource;
 use Iyzipay\Model\Locale;
 use Iyzipay\Request;
+use Iyzipay\Options;
 
 class IyzipayResourceTest extends TestCase
 {
     public function test_should_get_http_headers()
     {
+        $options = new Options();
         $request = new Request();
         $request->setLocale(Locale::TR);
         $request->setConversationId("123456");
+        $url = $options->getBaseUrl() . "/payment/test";
 
-        $headers = parent::callMethod(new IyzipayResource(), "getHttpHeaders", array($request, $this->options));
-
+        $headers = parent::callMethod(new IyzipayResource(), "getHttpHeadersV2", array($url, $request, $this->options));
         $this->assertNotEmpty($headers);
-        $this->assertEquals(5, count($headers));
+        $this->assertEquals(4, count($headers));
         $this->assertEquals("Accept: application/json", $headers[0]);
         $this->assertEquals("Content-type: application/json", $headers[1]);
         $this->assertStringStartsWith("Authorization: ", $headers[2]);
-        $this->assertStringStartsWith("x-iyzi-rnd: ", $headers[3]);
-        $this->assertStringStartsWith("x-iyzi-client-version: ", $headers[4]);
+        $this->assertStringStartsWith("x-iyzi-client-version: ", $headers[3]);
     }
 
     public function test_should_prepare_authorization_string()
     {
+        $options = new Options();
         $request = new Request();
         $request->setLocale(Locale::TR);
         $request->setConversationId("123456");
+        $uri = $options->getBaseUrl() . "/payment/test";
+        $expected_hash = 'IYZWSv2 YXBpS2V5OmFwaUtleSZyYW5kb21LZXk6cm5kJnNpZ25hdHVyZTpkMDBiYWE0YjZkYzU5MzA0NzJlNGQyMmEzYzUwZmIwN2RlNTBiMGRmYjhmNWFmNWI5ZjczMTU4ZDZiMTg0ZmY2';
 
-        $str = parent::callMethod(new IyzipayResource(), "prepareAuthorizationString", array($request, $this->options, "rnd"));
-
-        $this->assertEquals("IYZWS apiKey:opW0tI3yAVjzROFRTgHjIGsriHw=", $str);
+        $str = parent::callMethod(new IyzipayResource(), "prepareAuthorizationStringV2", array($uri, $request, $this->options, "rnd"));
+        $this->assertEquals($expected_hash, $str);
     }
 }
